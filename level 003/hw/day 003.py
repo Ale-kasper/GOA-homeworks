8f5c27e6a7970b1869b1b043d255ba77c294c8d9
Ale-kasper <gelovanialexandra@gmail.com>
2025-07-10 19:58:55 +0400

    second commit

---
+++ level 003/hw/day 003.py
@@ -0,0 +1,15 @@
+name = input("enter your name: ")
+print("hello" + " " + name) 
+
+#input rodesac adamiani wers rames compiutershi,
+#output = compiuters rac gamoaqvs.
+
+name = "Aleksandra"
+print(name)
+print(name)
+
+command = (input ("enter your word: "))
+print(command)
+print(command)
+
+
