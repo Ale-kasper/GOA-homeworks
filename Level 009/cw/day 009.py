f701337c5beb2adee08bdee04a9719478d1cf798
Ale-kasper <gelovanialexandra@gmail.com>
2025-07-31 21:42:33 +0400

    second commit

---
+++ Level 009/cw/day 009.py
@@ -0,0 +1,14 @@
+value_1 = '5'
+value_2 = '2'
+
+if value_1 > value_2:
+    print("iyidis vashls")
+
+elif 5 < 2:
+    print("iyidis msxals")
+
+elif 2 < 5:
+    print("iyidis vashls")
+
+else:
+    print("ver iyidis verafers")
