b3b3d58f980fd126d7802ca996b65330e7e80d60
Aleksandra Gelovani <gelovanialexandra@gmail.com>
2025-08-14 20:56:14 +0400

    Delete level 03 directory

--- level 03/cw/day 003.py
+++
@@ -1,8 +0,0 @@
-# inputexample = input ("ღირს თუ არა 2 კომპიუტერის ყიდვა?")
-
-# print(inputexample)
-
-inputexample = input("მუშაობს თუ არა ჩემი მაცივარი?")
-print(inputexample)
-
-inputexample = "444: "
